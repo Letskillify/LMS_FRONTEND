c7fb5a80b01aed4d9dee29105987708698f09f14
mohini64 <mohinigurjar213@gmail.com>
2025-02-01 18:03:27 +0530

    update file

--- src/Admin Components/Vouchars/ExpenseForm.jsx
+++ src/Admin Components/Vouchars/ExpenseForm.jsx
@@ -62,8 +62,8 @@ const validationSchema = Yup.object({
             unitPrice: Yup.number().min(0, 'Unit price must be at least 0').required('Unit price is required')
         })
     ),
-    additionalCharges: Yup.array().of(
-        Yup.object().shape({   
+    expenseItems: Yup.array().of(
+        Yup.object({
             chargeName: Yup.string().required('Charge name is required'),
             amount: Yup.number().min(0, 'Charge amount must be at least 0').required('Charge amount is required')
         })
